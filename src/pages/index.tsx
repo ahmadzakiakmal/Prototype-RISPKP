9102ae3952d9d63ac944bad7930138489d8ba370
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-09 17:40:38 +0700

    feat: create Map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import Layout from "@/components/Layout";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 export default function Home() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/Map"), {
+        ssr: false,
+        loading: () => <p>Map Loading...</p>,
+      }),
+    []
+  );
   return (
     <Layout>
-      <main className="text-black bg-blue-500/10 flex justify-center items-center h-full">test</main>
+      <main className="text-black bg-blue-500/10 flex justify-center items-center h-full">
+        <Map position={[-7.801363, 110.364787]} zoom={13} />
+      </main>
     </Layout>
   );
 }
