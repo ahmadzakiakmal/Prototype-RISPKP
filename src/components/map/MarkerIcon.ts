06f98de5a7b223ea2a3ca059b91a1041c0bc6af5
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-09 20:38:30 +0700

    fix: directory name

--- src/components/map/MarkerIcon.ts
+++ src/components/map/MarkerIcon.ts
@@ -1,14 +1,14 @@
 import L from "leaflet";
 
 export const normalMarkerIcon = new L.Icon({
-  iconUrl: "/svgs/MarkerNormal.png",
+  iconUrl: "/pngs/MarkerNormal.png",
   iconSize: [48, 48],
   iconAnchor: [24, 48],
   popupAnchor: [0, -48],
 });
 
 export const alertMarkerIcon = new L.Icon({
-  iconUrl: "/svgs/MarkerAlert.png",
+  iconUrl: "/pngs/MarkerAlert.png",
   iconSize: [48, 48],
   iconAnchor: [24, 48],
   popupAnchor: [0, -48],
