2e00fe0c5c0aab0b590e2a62fc85d4af873d379c
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-18 07:07:01 +0700

    feat: create RegionCard component

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -1,4 +1,26 @@
 import Layout from "@/components/Layout";
+import { BsFire } from "react-icons/bs";
+
+function RegionCard({ name }) {
+  return (
+    <div className="p-5 outline outline-1 outline-proto-200 shadow-md w-full rounded-[5px]">
+      <h1 className="font-bold">{name}</h1>
+      <hr className="my-1" />
+      <h2 className="font-semibold">Level Risiko:</h2>
+      <span className="text-green-600 font-bold text-[20px] flex items-center gap-2 mb-2">
+        N/A <BsFire />
+      </span>
+      <h2 className="font-semibold">Riwayat Kasus Kebakaran</h2>
+      <ul>
+        <li>2023: N/A</li>
+        <li>2022: N/A</li>
+        <li>2021: N/A</li>
+        <li>2020: N/A</li>
+        <li>2019: N/A</li>
+      </ul>
+    </div>
+  );
+}
 
 export default function DataPage() {
   return (
@@ -11,20 +33,15 @@ export default function DataPage() {
           <h1 className="text-3xl font-bold">Data</h1>
         </section>
 
-        <section className="bg-white p-5 w-full rounded-[10px] shadow-md">
-          <div className="p-5 outline outline-1 outline-proto-200 shadow-md w-fit rounded-[5px]">
-            <h1 className="font-semibold">Kota Yogyakarta</h1>
-            <hr />
-            <h2>Riwayat Kasus Kebakaran</h2>
-            <ul>
-              <li>2023: N/A</li>
-              <li>2022: N/A</li>
-              <li>2021: N/A</li>
-              <li>2020: N/A</li>
-              <li>2019: N/A</li>
-            </ul>
-          </div>
+        <section className="bg-white p-5 w-full rounded-[10px] shadow-md grid grid-cols-1 min-[425px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-10">
+          <RegionCard name="Kota Yogyakarta" />
+          <RegionCard name="Kabupaten Sleman" />
+          <RegionCard name="Kabupatan Bantul" />
+          <RegionCard name="Kabupaten Gunung Kidul" />
+          <RegionCard name="Kabupaten Kulon Progo" />
         </section>
+
+        
       </main>
     </Layout>
   );
