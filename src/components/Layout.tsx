01815b46b87dfbaab6d36d65c82badab10278f26
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-20 17:06:53 +0700

    feat: update Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,92 @@
+import { useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import Navbar from "./Navbar";
 import Link from "next/link";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { IoStatsChart } from "react-icons/io5";
-import { IoMdSettings } from "react-icons/io";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <main className="min-h-screen bg-proto-100 flex font-poppins">
-      <section className="bg-proto-300 flex-shrink-0">
-        <div className="flex justify-center items-center gap-3 border-b border-white py-10 px-5 lg:!p-10">
-          <div className="w-[30px] lg:w-[50px] aspect-square rounded-full bg-white" />
-          <h1 className="text-[28px] font-bold hidden lg:block">Prototype</h1>
-        </div>
-        
-        <div className="flex flex-col gap-5 py-10 px-5 ">
-          <Link href="/" className="flex justify-start px-4 items-center gap-3 hover:bg-white/10 py-2 rounded-[8px] transition">
-            <FaMapLocationDot className="text-[30px] flex-shrink-0" /> 
-            <span className="text-[18px] font-medium hidden lg:block">Map</span>
-          </Link>
-          <Link href="/data" className="flex justify-start px-4 items-center gap-3 hover:bg-white/10 py-2 rounded-[8px] transition">
-            <IoStatsChart className="text-[30px] flex-shrink-0" /> 
-            <span className="text-[18px] font-medium hidden lg:block">Data</span>
-          </Link>
-          <Link href="#" className="flex justify-start px-4 items-center gap-3 hover:bg-white/10 py-2 rounded-[8px] transition">
-            <IoMdSettings className="text-[30px] flex-shrink-0" /> 
-            <span className="text-[18px] font-medium hidden lg:block">Settings</span>
-          </Link>
+      <Navbar />
+
+      <section
+        className={
+          "bg-green-400 relative transition-[width] " +
+          (isMenuOpen ? "w-[270px]" : "w-0")
+        }
+      >
+        {/* open menu button */}
+        <button
+          className="absolute bg-green-400 left-[100%] top-[100px] w-10 aspect-square grid place-items-center rounded-r-[5px]"
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+          }}
+        >
+          <FaAngleDoubleRight
+            className={
+              "text-white text-[20px] transition " +
+              (isMenuOpen ? "rotate-180" : "")
+            }
+          />
+        </button>
+
+        <div
+          className={
+            "pt-[120px] px-3 !overflow-x-hidden " + (isMenuOpen ? "" : "!p-0")
+          }
+        >
+          <section
+            className={
+              "border-t-2 border-neutral-400 py-3 " +
+              (isMenuOpen ? "" : "hidden")
+            }
+          >
+            <h2 className="text-[14px] font-bold leading-[105%]">
+              ANALISIS RISIKO KEBAKARAN
+            </h2>
+            <ul className="flex flex-col text-[15px] mt-2 gap-2 leading-[105%] list-disc">
+              <li>
+                <Link href="#">Metode Analisis Risiko Kebakaran</Link>
+              </li>
+              <Link href="#">Peta Kepadatan Penduduk</Link>
+              <Link href="#">Peta Kepadatan Bangunan</Link>
+              <Link href="#">Peta Bahaya SPBU</Link>
+              <Link href="#">Peta Bahaya SPBU Mini</Link>
+              <Link href="#">Peta Jangkauan Pos</Link>
+              <Link href="#">Peta Kepadatan Komersial</Link>
+              <Link href="#">Peta Analisis Risiko Kebakaran</Link>
+            </ul>
+          </section>
+          <section
+            className={
+              "border-t-2 border-neutral-400 py-3 " +
+              (isMenuOpen ? "" : "hidden")
+            }
+          >
+            <h2 className="text-[14px] font-bold leading-[105%]">
+              STATISTIK KEJADIAN KEBAKARAN
+            </h2>
+          </section>
+          <section
+            className={
+              "border-t-2 border-neutral-400 py-3 " +
+              (isMenuOpen ? "" : "hidden")
+            }
+          >
+            <h2 className="text-[14px] font-bold leading-[105%]">
+              WILAYAH MANAJEMEN KEBAKARAN
+            </h2>
+          </section>
+          <section
+            className={
+              "border-t-2 border-neutral-400 py-3 " +
+              (isMenuOpen ? "" : "hidden")
+            }
+          >
+            <h2 className="text-[14px] font-bold leading-[105%]">
+              USER MANAGEMENT
+            </h2>
+          </section>
         </div>
       </section>
       <section className="w-full p-10">{children}</section>
