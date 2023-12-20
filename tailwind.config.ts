f52ba76812c8175ddbf801af36693de204bcf773
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-20 11:11:01 +0700

    feat: define new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,20 @@ const config: Config = {
           "100" : "#F8F9FA",
           "200" : "#DEDEDE",
           "300" : "#37474F",
-        }
+        },
+        neutral: {
+          "100" : "F8F9FA",
+          "200" : "#DEDEDE",
+          "300" : "#37474F",
+          "400" : "#23272A",
+        },
+        green : {
+          "100" : "#D4F7EC",
+          "200" : "#37D8A3",
+          "300" : "#19B285",
+          "400" : "#08A78B",
+        },
+        navy: "#1B5270",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
