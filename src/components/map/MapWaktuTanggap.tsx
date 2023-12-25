f7ac7c7f8354821f74d599249a02acceee4986db
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-25 20:16:57 +0700

    feat: create MapWaktuTanggap component

---
+++ src/components/map/MapWaktuTanggap.tsx
@@ -0,0 +1,63 @@
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import geojsonData from "@/data/Waktu_Tanggap.json";
+import { useEffect } from "react";
+
+export default function MapWaktuTanggap(props: any) {
+  const { zoom, position } = props;
+  const data: GeoJSON.GeoJsonObject = geojsonData as GeoJSON.GeoJsonObject;
+
+  useEffect(() => {
+    // @ts-expect-error: GeoJsonObject has no property features
+    data.features.forEach((f) => {
+      console.log(f.properties);
+    });
+  }, []);
+
+  return (
+    <MapContainer
+      className="w-full h-full select-none"
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">
+        OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeoJSON
+        // eslint-disable-next-line
+        data={data}
+        style={(feature) => {
+          const level = feature?.properties.gridcode;
+          return {
+            fillColor:
+              level == 1
+                ? "#74C24E"
+                : level == 2
+                  ? "#AEDE4E"
+                  : level == 3
+                    ? "#FFFF4D"
+                    : level == 4
+                      ? "#FFA64D"
+                      : "#FF4D4D",
+            fillOpacity: 0.65,
+            color: "#23272A",
+            weight: 2,
+          };
+        }}
+        onEachFeature={(feature, layer) => {
+          const desa = feature.properties.DESA;
+          const level = feature.properties.gridcode;
+          layer.bindTooltip(
+            `<span style="font-weight:600">${desa}</span><br/>
+          <span>Frekuensi: ${level}</span>`
+          );
+        }}
+      ></GeoJSON>
+    </MapContainer>
+  );
+}
