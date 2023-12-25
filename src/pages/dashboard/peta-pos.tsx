ad02f89f87a5a8f4eb7a049a6415a2ef2abdc873
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-25 20:43:06 +0700

    feat: create MapPos component

--- src/pages/dashboard/peta-pos.tsx
+++ src/pages/dashboard/peta-pos.tsx
@@ -1,9 +1,21 @@
 import Layout from "@/components/Layout";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
-export default function PetaSektorPage() {
+export default function BahayaSPBUMiniPage() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/map/MapPos"), {
+        ssr: false,
+        loading: () => <p>Loading...</p>,
+      }),
+    []
+  );
   return (
     <Layout>
-      <main className="w-full"></main>
+      <main className="w-full h-[800px]">
+        <Map zoom={14} position={[-7.801363, 110.364787]} />
+      </main>
     </Layout>
   );
 }
