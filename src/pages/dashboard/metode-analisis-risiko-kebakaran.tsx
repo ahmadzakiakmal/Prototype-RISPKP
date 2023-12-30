009baa9f10a2696dd07dd4d3bef777f6843b850f
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-30 20:03:23 +0700

    feat: add referensi (fixes issue #7)
    added referensi for metode analisis page

--- src/pages/dashboard/metode-analisis-risiko-kebakaran.tsx
+++ src/pages/dashboard/metode-analisis-risiko-kebakaran.tsx
@@ -4,15 +4,19 @@ export default function MetodeAnalisisPage() {
   return (
     <Layout>
       <main>
-        <h1 className="text-xl sm:text-2xl font-bold">Metode Analisis Risiko Kebakaran</h1>
-        <section className="overflow-x-scroll">
+        <h1 className="text-xl sm:text-2xl font-bold">
+          Metode Analisis Risiko Kebakaran
+        </h1>
+        <section className="overflow-x-auto">
           <table className="w-full border-2 border-neutral-400 mt-5 min-w-[700px] text-[14px] sm:text-[16px]">
             <tbody>
               <tr className="bg-green-300">
                 <th className="border-r-2 border-neutral-400 py-2">Kelas</th>
                 <th className="border-r-2 border-neutral-400 py-2">Variabel</th>
                 <th className="border-r-2 border-neutral-400 py-2">Bobot</th>
-                <th className="border-r-2 border-neutral-400 py-2">Klasifikasi</th>
+                <th className="border-r-2 border-neutral-400 py-2">
+                  Klasifikasi
+                </th>
                 <th>Keterangan</th>
               </tr>
               <RowItem
@@ -150,6 +154,29 @@ export default function MetodeAnalisisPage() {
               />
             </tbody>
           </table>
+        </section>
+
+        <section className="mt-5">
+          <h1 className="text-lg sm:text-xl font-bold">Referensi</h1>
+          <ul className="list-disc pl-5 mt-2">
+            <li className="text-justify">
+              Priambodo, D. (2018) Penentuan Nilai Jarak Aman
+              SumberTidakBergerak: Skenario Kebakaran dan Ledakan pada SPBU dan
+              SPPBE di Sekitar Tapak RDE. Jurnal Pengembangan EnergiNuklir;
+              <br />
+              Yulianto, B., Tejamaya, M., (2022) AnalisisRisiko Kebakaran dan
+              Ledakan Kegiatan Unloading Bahan Bakar Minyak di SPBU dengan
+              menggunakan Dow's Fire & Explosion Index, ALOHA dan MARPLOT.
+              Jurnal Ilmu Kesehatan Masyarakat; dan Sukma, A.J., Cahyono, R.B.,
+              (2017) Analisis risiko potensi bahaya kebakaran dan ledakan
+              beserta dampaknya pada pekerja dan masyarakat di PT.Pertamina -
+              Terminal BBM Rewulu. Journal of Community Medicine and Public
+              Health
+            </li>
+            <li className="text-justify">Peraturan BNPB No 2 Tahun 2012</li>
+            <li className="text-justify">Permen PUPR No 20/PRT/M/2009</li>
+            <li className="text-justify">SNI 03-1733-2004</li>
+          </ul>
         </section>
       </main>
     </Layout>
