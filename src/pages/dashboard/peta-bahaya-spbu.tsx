a88be44b98176056faed018f90763211eaeef1de
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-02 09:18:31 +0700

    feat: add map legend in Sebaran SPBU (issue #4)

--- src/pages/dashboard/peta-bahaya-spbu.tsx
+++ src/pages/dashboard/peta-bahaya-spbu.tsx
@@ -15,8 +15,23 @@ export default function BahayaSPBUPage() {
     <Layout>
       <main className="w-full">
         <h1 className="text-xl sm:text-2xl font-bold mb-5">Peta Sebaran SPBU</h1>
-        <section className="h-[80vh] max-h-[800px]">
+        <section className="h-[80vh] max-h-[800px] relative">
           <Map position={[-7.801363, 110.364787]} zoom={14} />
+
+          <section className="absolute bottom-0 left-0 p-[15px] bg-white outline outline-1 m-5 !z-[10] flex flex-col gap-1">
+            <div className="text-[12px] flex items-center gap-3">
+              <div className="bg-[#5EBA30] w-[50px] h-[20px] outline outline-1 outline-slate-500" />
+              <p>Rendah</p>
+            </div>
+            <div className="text-[12px] flex items-center gap-3">
+              <div className="bg-[#FFFF30] w-[50px] h-[20px] outline outline-1 outline-slate-500" />
+              <p>Sedang</p>
+            </div>
+            <div className="text-[12px] flex items-center gap-3">
+              <div className="bg-[#FF3030] w-[50px] h-[20px] outline outline-1 outline-slate-500" />
+              <p>Tinggi</p>
+            </div>
+          </section>
         </section>
       </main>
     </Layout>
