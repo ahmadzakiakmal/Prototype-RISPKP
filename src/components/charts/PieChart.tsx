27d5a3be62e5fcb74b6cda7b79a75d63e142ea0c
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-02 18:19:41 +0700

    feat: create doughnut chart for Penyebab Kebakaran

---
+++ src/components/charts/PieChart.tsx
@@ -0,0 +1,61 @@
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  // Legend,
+  ArcElement,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  // Legend,
+  ArcElement
+);
+
+export default function PieChart() {
+  const data = {
+    labels: [
+      "Arus Pendek",
+      "Kecelakaan",
+      "Putung Rokok",
+      "Bakar Sampah",
+      "Kesengajaan",
+      "Selang Bensin Lepas",
+      "Kegiatan Dapur",
+      "Percikan Las",
+      "Tidak Diketahui",
+      "Human Error",
+      "Pletikan Kembang Api",
+    ],
+    datasets: [
+      {
+        label: "Data Penyebab Kebakaran",
+        data: [81.40, 39.19, 25.12, 24.12, 24.12, 3.1, 3.1, 2.1, 1.1, 1.1, 1.0],
+        backgroundColor: [
+          "#003E5D",
+          "#68E7EA",
+          "#FEDC6D",
+          "#FE9C00",
+          "#FFCFAD",
+          "#A63945",
+          "#43682B",
+          "#F9BA54",
+          "#FF4F6F",
+          "#600222",
+          "#006F65"
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  return <Doughnut data={data} />;
+}
--- src/pages/dashboard/statistik-ketangguhan-kebakaran.tsx
+++ src/pages/dashboard/statistik-ketangguhan-kebakaran.tsx
@@ -1,9 +1,15 @@
 import Layout from "@/components/Layout";
+import PieChart from "@/components/charts/PieChart";
 
 export default function StatistikKetangguhanKebakaranPage() {
   return (
     <Layout>
-      <main className="w-full"></main>
+      <h1 className="text-xl sm:text-2xl font-bold mb-5">
+        Akumulasi Penyelamatan
+      </h1>
+      <main className="w-[400px]">
+        <PieChart />
+      </main>
     </Layout>
   );
 }
