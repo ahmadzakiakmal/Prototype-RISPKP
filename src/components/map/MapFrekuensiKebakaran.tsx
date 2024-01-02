1e5b87152c42ca5e4abeda2e20c394dce688d863
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-02 10:27:11 +0700

    feat: add map legend in Kejadian Kebakaran (issue #4)

--- src/components/map/MapFrekuensiKebakaran.tsx
+++ src/components/map/MapFrekuensiKebakaran.tsx
@@ -21,7 +21,7 @@ export default function MapFrekuensiKebakaran(props: any) {
 
   return (
     <MapContainer
-      className="w-full h-full select-none"
+      className="w-full h-full select-none z-[1]"
       center={position}
       zoom={zoom}
       scrollWheelZoom={false}
