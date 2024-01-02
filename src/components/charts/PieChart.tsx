01264287701b54e8f33faf2557bef51f0c73526a
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-03 06:04:37 +0700

    feat: separated data from component

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -9,6 +9,7 @@ import {
   Legend,
   ArcElement,
 } from "chart.js";
+import jsonData from "@/data/Persentase_Penyebab_Kebakaran.json";
 
 ChartJS.register(
   CategoryScale,
@@ -21,41 +22,7 @@ ChartJS.register(
 );
 
 export default function PieChart() {
-  const data = {
-    labels: [
-      "Arus Pendek",
-      "Kecelakaan",
-      "Putung Rokok",
-      "Bakar Sampah",
-      "Kesengajaan",
-      "Selang Bensin Lepas",
-      "Kegiatan Dapur",
-      "Percikan Las",
-      "Tidak Diketahui",
-      "Human Error",
-      "Pletikan Kembang Api",
-    ],
-    datasets: [
-      {
-        label: "Data Penyebab Kebakaran",
-        data: [81.4, 39.19, 25.12, 24.12, 24.12, 3.1, 3.1, 2.1, 1.1, 1.1, 1.0],
-        backgroundColor: [
-          "#003E5D",
-          "#68E7EA",
-          "#FEDC6D",
-          "#FE9C00",
-          "#FFCFAD",
-          "#A63945",
-          "#43682B",
-          "#F9BA54",
-          "#FF4F6F",
-          "#600222",
-          "#006F65",
-        ],
-        hoverOffset: 4,
-      },
-    ],
-  };
+  const data = jsonData;
 
   return (
     <Doughnut
