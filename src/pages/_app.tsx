9eb4c1183856a272c60045ddc2c05c50ad4e278d
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-04 11:53:34 +0700

    feat: setup react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <main className="font-poppins text-neutral-400">
+      <ToastContainer position="bottom-right" />
       {router.pathname.includes("dashboard") ? <Navbar /> : null}
       <Component {...pageProps} />
     </main>
