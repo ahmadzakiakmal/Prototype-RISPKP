983470ebfa24161be1b769e3bdb3ab89e0d79c49
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-05 18:31:51 +0700

    feat: create Map component (issue #10)

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,74 +1,49 @@
 // src/components/Map.tsx
-import { Circle, MapContainer, Polygon, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import MapMarker from "./utilities/MapMarker";
+import Legend from "./utilities/Legend";
+import useDynamicZoom from "@/hooks/useDynamicZoom";
+import DynamicZoom from "./utilities/DynamicZoom";
 
-export default function Map(props: any) {
-  const { position, zoom } = props;
+interface MapProps {
+  geoJsonData: GeoJSON.GeoJsonObject;
+  LegendProps: {
+    title: string;
+    colorArr: string[];
+    labelArr: string[];
+  };
+  geoJsonStyle: (feature: any) => any;
+  geoJsonOnEachFeature: (feature: any, layer: any) => any;
+}
 
-  const locations = [
-    {
-      name: "Titik 0 Yogya",
-      position: [-7.801363, 110.364787],
-      status: "normal",
-    },
-    {
-      name: "GSP UGM",
-      position: [-7.7700939, 110.3778412],
-      status: "alert",
-    },
-    {
-      name: "SGLC FT UGM",
-      position: [-7.7653606, 110.3723441],
-      status: "normal",
-    },
-  ];
-
-  const ringroad = [
-    [-7.7435586, 110.3441336],
-    [-7.7536676,110.3812658],
-    [-7.7597507,110.3882904],
-    [-7.7584691,110.3994268],
-    [-7.7659146, 110.4304312],
-    [-7.7845781, 110.4285852],
-    [-7.7836822, 110.4103641],
-    [-7.8218642,110.4098009],
-    [-7.837601478246318, 110.40618475337406],
-    [-7.8362169, 110.389819],
-    [-7.8341676, 110.3879389],
-    [-7.8365604, 110.3761791],
-    [-7.8332351, 110.3579558],
-    [-7.8288014, 110.3565482],
-    [-7.8218442, 110.3237569],
-    [-7.8019574, 110.3241029],
-    [-7.7997382, 110.3285112],
-    [-7.7784811, 110.3305039],
-  ];
-
+export default function Map(props: MapProps) {
+  const zoom = useDynamicZoom();
   return (
-    <MapContainer
-      className="w-full h-full select-none"
-      center={position}
-      zoom={zoom}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <section className="h-[80vh] max-h-[800px] relative">
+      <MapContainer
+        className="w-full h-full select-none relative z-[1]"
+        center={[-7.801363, 110.364787]}
+        zoom={14}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <DynamicZoom zoom={zoom} />
+        <GeoJSON 
+          data={props.geoJsonData}
+          style={props.geoJsonStyle}
+          onEachFeature={props.geoJsonOnEachFeature}
+        />
+      </MapContainer>
+      <Legend
+        title={props.LegendProps.title}
+        colorArr={props.LegendProps.colorArr}
+        labelArr={props.LegendProps.labelArr}
       />
-      {locations.map((location, index) => (
-        <MapMarker key={index} {...location} />
-      ))}
-      <Circle
-        center={[-7.7700939, 110.3778412]}
-        radius={300}
-        color="rgba(255,0,0)"
-      ></Circle>
-      {/* //eslint-disable 
-          @ts-ignore */}
-      <Polygon positions={[...ringroad]} fillColor="#3388FF" fillOpacity={.1} />
-    </MapContainer>
+    </section>
   );
 }
--- src/pages/dashboard/peta-jangkauan-pos.tsx
+++ src/pages/dashboard/peta-jangkauan-pos.tsx
@@ -2,22 +2,48 @@ import Layout from "@/components/Layout";
 import Loading from "@/components/Loading";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
+import data from "@/data/Cakupan_Pos.json";
 
 export default function JangkauanPosPage() {
   const Map = useMemo(
     () =>
-      dynamic(() => import("@/components/map/MapJangkauanPos"), {
+      dynamic(() => import("@/components/map/Map"), {
         ssr: false,
         loading: () => <Loading />,
       }),
     []
   );
+  const geoJsonData = data as GeoJSON.GeoJsonObject;
+  const LegendProps = {
+    title: "Cakupan Pos",
+    colorArr: ["bg-[#FAD155]", "bg-[#F2A72E]", "bg-[#AD5313]"],
+    labelArr: ["Rendah", "Sedang", "Tinggi"],
+  };
+
   return (
     <Layout>
-      <main className="w-full">
-        <section className="h-[80vh] max-h-[800px]">
-          <Map position={[-7.801363, 110.364787]} zoom={14} />
-        </section>
+      <main className="h-[80vh] max-h-[800px] relative">
+        <h1 className="text-xl sm:text-2xl font-bold mb-5">Jangkauan Pos</h1>
+        <Map
+          geoJsonData={geoJsonData}
+          LegendProps={LegendProps}
+          geoJsonStyle={(feature) => {
+            const level = feature?.properties.gridcode;
+            return {
+              fillColor:
+                level == 3 ? "#FAD155" : level == 2 ? "#F2A72E" : "#000",
+              fillOpacity: 0.65,
+              color: "#23272A",
+              weight: 2,
+            };
+          }}
+          geoJsonOnEachFeature={(feature, layer) => {
+            const sektor = feature.properties.Sektor;
+            const level = feature.properties.Klas_ha;
+            layer.bindTooltip(`<span style="font-weight:600">${sektor}</span><br/>
+            <span>Kepadatan Penduduk: ${level}</span>`);
+          }}
+        />
       </main>
     </Layout>
   );
