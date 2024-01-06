4679ebb234fead3177a73df24afbbf84dc5e9282
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-06 18:10:48 +0700

    feat: get user data from the server (issue #1 )

--- src/pages/admin/user-management.tsx
+++ src/pages/admin/user-management.tsx
@@ -1,4 +1,7 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function UserManagementPage() {
   interface User {
@@ -6,19 +9,38 @@ export default function UserManagementPage() {
     role: string;
     username: string;
   }
-  
-  const users: User[] = [
-    {
-      _id: 1,
-      username: "admin",
-      role: "Admin"
-    },
-    {
-      _id: 2,
-      username: "user",
-      role: "User"
-    }
-  ];
+
+  const [users, setUsers] = useState([] as User[]);
+
+  const getUsers = () => {
+    const toastLoading = toast.loading("Mengambil data...");
+    axios
+      .get(process.env.NEXT_PUBLIC_API_URL + "users", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        toast.update(toastLoading, {
+          render: "Berhasil",
+          type: "success",
+          isLoading: false,
+          autoClose: 2000,
+        });
+        return;
+      })
+      .catch(() => {
+        toast.update(toastLoading, {
+          render: "Gagal",
+          type: "error",
+          isLoading: false,
+          autoClose: 2000,
+        });
+      });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <Layout>
@@ -26,24 +48,31 @@ export default function UserManagementPage() {
         <h1 className="text-xl sm:text-2xl font-bold mb-5">User Management</h1>
         <section className="w-full">
           <div className="flex flex-row items-center justify-between w-full border-t border-gray-300" />
-          {
-            users.map((user) => (
-              <div key={user._id} className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-gray-300">
-                <div className="flex flex-row items-center">
-                  <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-                  <div className="ml-3">
-                    <p className="font-semibold">{user.username}</p>
-                    <p className="text-sm text-gray-500">User ID: {user.username}</p>
-                    <p className="text-sm text-gray-500">User ID: {user._id}</p>
-                  </div>
-                </div>
-                <div className="flex flex-row items-center">
-                  <button className="px-3 py-1 transition rounded-md bg-green-500 hover:bg-green-800 text-white">Edit</button>
-                  <button className="px-3 py-1 transition rounded-md bg-red-500 hover:bg-red-800 text-white ml-2">Delete</button>
+          {users.map((user) => (
+            <div
+              key={user._id}
+              className="flex flex-row items-center justify-between w-full px-5 py-3 border-b border-gray-300"
+            >
+              <div className="flex flex-row items-center">
+                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                <div className="ml-3">
+                  <p className="font-semibold">{user.username}</p>
+                  <p className="text-sm text-gray-500">
+                    Role: {user.role}
+                  </p>
+                  <p className="text-sm text-gray-500">ID: {user._id}</p>
                 </div>
               </div>
-            ))
-          }
+              <div className="flex flex-row items-center">
+                <button className="px-3 py-1 transition rounded-md bg-green-500 hover:bg-green-800 text-white">
+                  Edit
+                </button>
+                <button className="px-3 py-1 transition rounded-md bg-red-500 hover:bg-red-800 text-white ml-2">
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
         </section>
       </main>
     </Layout>
