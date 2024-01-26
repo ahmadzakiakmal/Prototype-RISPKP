ed78bdd3ef247a3ee0be72fedd5da95dfd93799c
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-26 09:00:42 +0700

    feat: update Map component to support markers

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,24 +1,33 @@
 // src/components/Map.tsx
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  Marker,
+  Tooltip,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import Legend from "./utilities/Legend";
 import useDynamicZoom from "@/hooks/useDynamicZoom";
 import DynamicZoom from "./utilities/DynamicZoom";
+import dynamic from "next/dynamic";
+import { Icon } from "leaflet";
 
 interface MapProps {
-  geoJsonData: GeoJSON.GeoJsonObject;
-  LegendProps: {
+  geoJsonData?: GeoJSON.GeoJsonObject;
+  LegendProps?: {
     title: string;
     colorArr: string[];
     labelArr: string[];
   };
-  geoJsonStyle: (feature: any) => any;
-  geoJsonOnEachFeature: (feature: any, layer: any) => any;
+  geoJsonStyle?: (feature: any) => any;
+  geoJsonOnEachFeature?: (feature: any, layer: any) => any;
+  markers?: any;
 }
 
-export default function Map(props: MapProps) {
+function Map(props: MapProps) {
   const zoom = useDynamicZoom();
   return (
     <section className="h-[80vh] max-h-[800px] relative">
@@ -33,17 +42,43 @@ export default function Map(props: MapProps) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <DynamicZoom zoom={zoom} />
-        <GeoJSON 
-          data={props.geoJsonData}
-          style={props.geoJsonStyle}
-          onEachFeature={props.geoJsonOnEachFeature}
-        />
+        {props.geoJsonData && (
+          <GeoJSON
+            data={props?.geoJsonData}
+            style={props?.geoJsonStyle}
+            onEachFeature={props.geoJsonOnEachFeature}
+          />
+        )}
+        {props.markers &&
+          props.markers.map((marker: any) => {
+            return (
+              <Marker
+                position={marker.position}
+                icon={
+                  new Icon({
+                    iconUrl: marker.iconUrl,
+                    iconSize: marker.iconSize,
+                    iconAnchor: marker.iconAnchor,
+                  })
+                }
+                key={marker.key}
+              >
+                <Tooltip children={marker.children} offset={marker.popupAnchor} />
+              </Marker>
+            );
+          })}
       </MapContainer>
-      <Legend
-        title={props.LegendProps.title}
-        colorArr={props.LegendProps.colorArr}
-        labelArr={props.LegendProps.labelArr}
-      />
+      {props.LegendProps && (
+        <Legend
+          title={props.LegendProps.title}
+          colorArr={props.LegendProps.colorArr}
+          labelArr={props.LegendProps.labelArr}
+        />
+      )}
     </section>
   );
 }
+
+export default dynamic(() => Promise.resolve(Map), {
+  ssr: false,
+});
